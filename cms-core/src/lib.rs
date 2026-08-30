273e7735a40d3bf850c7b2440b63b353e94a09f2
Patryk Sikorski <sikor87@hotmail.com>
2026-08-30 03:32:48 +0100

    seting the db

--- cms-core/src/lib.rs
+++ cms-core/src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use serde::{Deserialize, Serialize};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Post {
+    pub id: i64,
+    pub slug: String,
+    pub title: String,
+    pub body_html: String,
+    pub status: String,
 }
